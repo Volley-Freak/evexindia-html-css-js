918b15bcd2421123679bede38aa3f163a2245f10
Volley-Freak <hrithikgusain07@gmail.com>
2022-04-06 04:59:04 +0530

    home page updated

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -23,3 +20,77 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// PRODUCT SWIPER
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// PRODUCT SWIPER
+
+// HIGHLIGHTS
+
+var mySwiper = new Swiper(".swiper-container", {
+  speed: 400,
+  spaceBetween: 100,
+  initialSlide: 0,
+  //truewrapper adoptsheight of active slide
+  autoHeight: false,
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // delay between transitions in ms
+  autoplay: 5000,
+  autoplayStopOnLast: true, // loop false also
+  // If we need pagination
+  pagination: ".swiper-pagination",
+  paginationType: "bullets",
+
+  // Navigation arrows
+  nextButton: ".swiper-button-next",
+  prevButton: ".swiper-button-prev",
+
+  // And if we need scrollbar
+  //scrollbar: '.swiper-scrollbar',
+  // "slide", "fade", "cube", "coverflow" or "flip"
+  effect: "slide",
+  // Distance between slides in px.
+  spaceBetween: 10,
+  //
+  slidesPerView: 4,
+  //
+  centeredSlides: true,
+  //
+  slidesOffsetBefore: 0,
+  //
+  grabCursor: true,
+});
+
+// TESTIMONIAL
+$(".team-slider").owlCarousel({
+  loop: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  smartSpeed: 450,
+  margin: 20,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    991: {
+      items: 3,
+    },
+    1200: {
+      items: 3,
+    },
+    1920: {
+      items: 3,
+    },
+  },
+});
